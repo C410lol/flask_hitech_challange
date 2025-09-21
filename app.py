4d35927f320061f0a70443dc4d0462017dd748c6
C410lol <gcaio7463@gmail.com>
2025-09-21 18:01:40 -0300

    Adicionando rota GET '/info'

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
 from flask import Flask
 
-app = Flask (__name__);
+app = Flask(__name__);
 
-@app.route ("/")
-def hello ():
+
+
+
+@app.route("/")
+def default():
     return "Hello, World!"
 
+@app.route("/info", methods=['GET'])
+def getRoute():
+    data = {
+        "name": "Caio Gomes",
+        "age": 18,
+        "department": "Projetos",
+        "function": "Assessor",
+        "startDate": "01/04/2025" 
+    }
+    return data
+
+
+
+
 if __name__ == "__main__":
-    app.run (host = "0.0.0.0", port = 5000, debug = True)
+    app.run(host = "0.0.0.0", port = 5000, debug = True)
